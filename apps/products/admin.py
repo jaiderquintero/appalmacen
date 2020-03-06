244d8833f8ca1afe9fdd5cd8ef2f423a855325ca
jaiderquintero <jquintero@uniguajira.edu.co>
2020-03-06 09:04:26 -0500

    registro modelo products y productype en apps/products/admin

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from apps.products.models import ProductType, Product
+
+admin.site.register(ProductType)
+admin.site.register(Product)
