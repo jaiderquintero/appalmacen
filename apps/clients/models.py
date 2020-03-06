037ee39e62999bbe4d6c36410ea39bc49f71c495
jaiderquintero <jquintero@uniguajira.edu.co>
2020-03-06 07:16:53 -0500

    Definir Modelo apps/clients/models

--- apps/clients/models.py
+++ apps/clients/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+# Create your models here.
+
+class Client(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Nombre")
+    lastname = models.CharField(max_length=50, verbose_name="Apellidos")
+    rut = models.CharField(max_length=20, verbose_name="Cedula")
+    address = models.CharField(max_length=50, verbose_name="Direccion")
+    phone = models.CharField(max_length=50, verbose_name="Telefono")
+    #email = models.CharField(max_length=50, verbose_name="Correo Electronico")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "cliente"
+        verbose_name_plural = "clientes"
