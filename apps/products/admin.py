ad79007219caf854841c72dd89bf8dfe99094d8c
jaiderquintero <jquintero@uniguajira.edu.co>
2020-03-26 18:40:55 -0500

    Config admin en sales

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from apps.products.models import ProductType, Product
 
 class ProductAdmin(admin.ModelAdmin):
-    readonly_fields = ('created', 'updated') #No permite edicion de create y update
+    # readonly_fields = ('created', 'updated') #No permite edicion de create y update
     list_display = ('name', 'quantity', 'price', 'description', 'mark', 'product_types')
     ordering = ('price','name','mark',
                 'product_types') # En caso que sea una sola ordering('column',)
