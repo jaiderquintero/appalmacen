919ddc9d2515c21a82a8dc89343533aa98529964
jaiderquintero <jquintero@uniguajira.edu.co>
2020-04-17 08:21:51 -0500

    template listClientes, urls y vista listClinets

--- apps/clients/urls.py
+++ apps/clients/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from apps.clients.views import index, listClients
 
 app_name ='clients'
 urlpatterns = [
-    path('', index, name='clients'),  # Donde index es el metodo o vista que se encuentra en app/clients/index
+    path('listar', listClients, name='listClients'),  # Donde index es el metodo o vista que se encuentra en app/clients/index
 
 ]
--- apps/clients/views.py
+++ apps/clients/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from apps.clients.models import Client
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'clients/index.html')
+
+def listClients(request):
+    client = Client.objects.all().order_by('-id')
+    context = {'clients':client}
+    return render(request, 'clients/listClients.html', context)
